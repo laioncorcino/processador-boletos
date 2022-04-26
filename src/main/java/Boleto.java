208ac6ad053a040320a63d6afdb9695f821eb019
Laion Corcino <laioncorcino@gmail.com>
2022-04-25 21:44:40 -0300

    feat: contiuando com classe boleto e fatura

--- src/main/java/Boleto.java
+++ src/main/java/Boleto.java
@@ -1,2 +1,15 @@
+import java.util.Date;
+
 public class Boleto {
+
+    private final long valorPago;
+
+    public Boleto(Date data, long valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public long getValorPago() {
+        return this.valorPago;
+    }
+
 }
--- src/main/java/Fatura.java
+++ src/main/java/Fatura.java
@@ -1,2 +1,32 @@
+import java.util.Date;
+
 public class Fatura {
+
+    private FaturaStatus status;
+    private Date data;
+    private long valor;
+    private String cliente;
+
+    public Fatura(Date data, long valor, String cliente) {
+        this.data = data;
+        this.valor = valor;
+        this.cliente = cliente;
+    }
+
+    public long getValor() {
+        return this.valor;
+    }
+
+    public FaturaStatus getStatus() {
+        return this.status;
+    }
+
+    public void pagaFatura(Pagamento pagamento) {
+        if (pagamento.getValorTotal() >= this.valor) {
+            this.status = FaturaStatus.PAGO;
+        } else {
+            this.status = FaturaStatus.NAO_PAGO;
+        }
+    }
+
 }
